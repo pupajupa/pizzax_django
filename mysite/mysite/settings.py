8ef82b5bead7f4443e0e5c5092fcc3a8983b394d
pupajupa <garilarulit@gmail.com>
2024-01-25 04:38:23 +0300

    LOGIN_AND_LOGOUT_PAGES

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -127,3 +127,7 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'myapp:pizzas_list'
+
+LOGOUT_REDIRECT_URL = 'myapp:pizzas_list'
--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from users import views
+from django.contrib.auth.views import LoginView,LogoutView
 
 app_name = "users"
 
 urlpatterns = [
     path("register/",views.register,name="register"),
+    path("login/",LoginView.as_view(template_name="users/login.html"),name="login"),
+    path("logout/",LogoutView.as_view(template_name="users/logout.html"),name="logout"),
 ]
    
