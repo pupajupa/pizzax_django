db24ffea3494b04deb99f41820604a93df0a6397
pupajupa <garilarulit@gmail.com>
2024-02-05 20:20:01 +0300

    UPDATE_PROFILE

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import new_user_form
+from .forms import new_user_form, update_profile_form
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 
 def register(request):
@@ -15,5 +17,16 @@ def register(request):
     return render(request, "users/register.html", context)
 
 
+@login_required
 def profile(request):
+    if request.method == "POST":
+        contact_number = request.POST.get("number")
+        name = request.POST.get("name")
+        last_name = request.POST.get("last_name")
+        image = request.FILES["upload"]
+        item = Profile(
+            contact_number=contact_number, image=image, name=name, last_name=last_name
+        )
+        item.save()
+        return redirect("/myapp/")
     return render(request, "users/profile.html")
